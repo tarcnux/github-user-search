f48ff1363a62468f0ed33bc03984673bb58bb745
Tarcisio Nunes <github@tarcnux.com.br>
2021-02-17 18:24:32 -0300

    Search component

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import './styles.scss';
 
 const Search = () => (
-    <main>
-        <h1>SEARCH</h1>
-    </main>
+    <header className="header-container">
+        <h1>Encontre um perfil no Github</h1>
+        <input 
+            type="text" name="github_user_name"
+            placeholder="Usuário Github"
+        />
+        <button>Encontrar</button>
+    </header>
 );
 
 export default Search;
