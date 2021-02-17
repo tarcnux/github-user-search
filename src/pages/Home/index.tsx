ae0a3048e40468104ae3035a3a9de1feefb1195f
Tarcisio Nunes <github@tarcnux.com.br>
2021-02-17 18:23:35 -0300

    Add Home Component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,10 +3,27 @@ import { Link } from 'react-router-dom';
 import './styles.scss';
 
 const Home = () => (
-    <main>
-        <h1>HOME</h1>
+    <main className="home-container">
+        <h1>
+            Desafio do Capítulo 3, <br />
+            Bootcamp DevSuperior
+        </h1>
+        <p>
+            Bem-vindos ao desafio do capítulo 3 do Bootcamp DevSuperior.
+        </p>
+
+        <p>
+            Favor observar as instruções passadas no capítulo sobre a <br />
+        elaboração deste projeto.
+        </p>
+
+        <p>
+            Este design foi adaptado a partir de Ant Design System for Figma, de <br />
+            Mateusz Wierzbicki: 
+            <a href="mailto:antforfigma@gmail.com"> antforfigma@gmail.com</a>
+        </p>
         <Link to="/search">
-            <p>Search</p>
+            <button>Começar</button>
         </Link>
     </main>
 );
